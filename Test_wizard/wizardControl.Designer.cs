16055727f715d764022538a1ede2f5e5c5e45219
fsprogamer <fsprogamer@gmail.com>
2016-09-05 18:03:42 +0300

    1

---
+++ Test_wizard/wizardControl.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Test_wizard
+{
+    partial class MainWizard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wizardControl = new AeroWizard.WizardControl();
+            ((System.ComponentModel.ISupportInitialize)(this.wizardControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // wizardControl
+            // 
+            this.wizardControl.BackColor = System.Drawing.Color.White;
+            this.wizardControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wizardControl.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wizardControl.Location = new System.Drawing.Point(0, 0);
+            this.wizardControl.Name = "wizardControl";
+            this.wizardControl.Size = new System.Drawing.Size(574, 415);
+            this.wizardControl.TabIndex = 0;
+            // 
+            // MainWizard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 415);
+            this.Controls.Add(this.wizardControl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "MainWizard";
+            this.Load += new System.EventHandler(this.MainWizard_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.wizardControl)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AeroWizard.WizardControl wizardControl;
+    }
+}
---
+++ Test_wizard/wizardControl.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows.Forms;
+using AeroWizard;
+
+namespace Test_wizard
+{
+    public partial class MainWizard : Form
+    {
+        int page_index = 0;
+        const string FlowPanelName = "FlowPanel";
+
+        public MainWizard()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWizard_Load(object sender, EventArgs e)
+        {
+
+            page_index = 0;
+            WizardPage page = new WizardPage();
+            page.Text = page_index.ToString();
+            page.Commit += wizardPage_Commit;
+                  
+            wizardControl.Pages.Add(page);
+
+            FlowLayoutPanel flpanel = new FlowLayoutPanel();
+
+            flpanel.FlowDirection = FlowDirection.TopDown;
+            flpanel.AutoSize = true;
+            flpanel.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            flpanel.WrapContents = true;
+            flpanel.Dock = DockStyle.Fill;
+            flpanel.Name = FlowPanelName;
+
+            wizardControl.SelectedPage.Controls.Add(flpanel);
+
+            for (int i = 0; i < 10; i++)
+            {
+                RadioButton box = new RadioButton();
+                box.Text = i.ToString();
+                box.AutoSize = true;
+                box.Dock = System.Windows.Forms.DockStyle.Fill;
+                box.Tag = i;
+                flpanel.Controls.Add(box);
+            }
+
+            page = new WizardPage();
+            wizardControl.Pages.Add(page);
+
+            wizardControl.RestartPages();
+        }
+
+        void wizardPage_Commit(object sender, AeroWizard.WizardPageConfirmEventArgs e)
+        {          
+            //int forms_visit_id = 0;
+
+            FlowLayoutPanel flpanel = (FlowLayoutPanel)e.Page.Controls[FlowPanelName];
+            RadioButton rb = new RadioButton();
+
+            if (1 != 0)
+            {
+                
+                page_index++;
+                wizardControl.Pages[page_index].Text = page_index.ToString();
+                wizardControl.Pages[page_index].Commit += wizardPage_Commit;
+                //wizardControl.Pages[page_index].Initialize += wizardPage_Initialize;
+
+                FlowLayoutPanel flpanel_next = new FlowLayoutPanel();
+                flpanel_next.FlowDirection = FlowDirection.TopDown;
+                flpanel_next.AutoSize = true;
+                flpanel_next.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+                flpanel_next.WrapContents = true;
+                flpanel_next.Dock = DockStyle.Fill;
+                flpanel_next.Name = FlowPanelName;
+
+                wizardControl.Pages[page_index].Controls.Add(flpanel_next);
+
+                for (int i=0; i<10; i++)
+                {
+                    RadioButton box = new RadioButton();
+                    box.Text = i.ToString();
+                    box.AutoSize = true;
+                    box.Dock = System.Windows.Forms.DockStyle.Fill;
+                    box.Tag = i;
+                    flpanel_next.Controls.Add(box);
+                }
+
+                WizardPage page = new WizardPage();
+                wizardControl.Pages.Add(page);
+
+                //wizardControl.PreviousPage();
+                wizardControl.RestartPages();
+            }
+            else
+            {
+                WizardPage finish_page = new WizardPage();
+                finish_page.IsFinishPage = true;
+                wizardControl.Pages.Add(finish_page);
+            }
+        }
+    }
+}
